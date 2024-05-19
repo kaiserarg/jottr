80a988cf992682abcfae61d5d46ae88ef133d42b
Brad Huang <ultravenusk1@gmail.com>
2024-05-19 01:09:15 -0400

    added slate, ts, shadcn

---
+++ editor/src/renderer/src/App.tsx
@@ -0,0 +1,17 @@
+import Landing from "./modules/Landing";
+import Editor from "./modules/Editor";
+
+function App(): JSX.Element {
+  return (
+    <>
+      <div>
+        <Landing />
+      </div>
+      <div>
+        <Editor />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ editor/src/renderer/src/modules/Landing.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarTrigger,
+  } from "@/components/ui/menubar"
+
+interface LandingProps {
+
+}
+
+const Landing: React.FC<LandingProps> = () => {
+    return (
+        <Menubar>
+            <MenubarMenu>
+                <MenubarTrigger>File</MenubarTrigger>
+                <MenubarContent>
+                <MenubarItem>
+                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>New Window</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Share</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Print</MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+                <MenubarTrigger>Edit</MenubarTrigger>
+                <MenubarContent>
+                <MenubarItem>
+                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>New Window</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Share</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Print</MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+                <MenubarTrigger>View</MenubarTrigger>
+                <MenubarContent>
+                <MenubarItem>
+                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>New Window</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Share</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Print</MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+        </Menubar>
+    );
+};
+
+export default Landing;
