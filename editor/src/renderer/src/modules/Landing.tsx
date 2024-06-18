e4fd6f168e046ad83a9ba6f3f9234fc90e9faf81
Anavaran <zyao2003@gmail.com>
2024-06-18 00:25:59 -0400

    changed container layouts

--- editor/src/renderer/src/modules/Landing.tsx
+++ editor/src/renderer/src/modules/Landing.tsx
@@ -35,81 +35,93 @@ interface LandingProps {
 
 const Landing: React.FC<LandingProps> = () => {
     return (
-        <main>
-            <Menubar>
-            <MenubarMenu>
-                <MenubarTrigger>File</MenubarTrigger>
-                <MenubarContent>
-                <MenubarItem>
-                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-                </MenubarItem>
-                <MenubarItem>New Window</MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem>Share</MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem>Print</MenubarItem>
-                </MenubarContent>
-            </MenubarMenu>
-            <MenubarMenu>
-                <MenubarTrigger>Edit</MenubarTrigger>
-                <MenubarContent>
-                <MenubarItem>
-                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-                </MenubarItem>
-                <MenubarItem>New Window</MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem>Share</MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem>Print</MenubarItem>
-                </MenubarContent>
-            </MenubarMenu>
-            <MenubarMenu>
-                <MenubarTrigger>View</MenubarTrigger>
-                <MenubarContent>
-                <MenubarItem>
-                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-                </MenubarItem>
-                <MenubarItem>New Window</MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem>Share</MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem>Print</MenubarItem>
-                </MenubarContent>
-            </MenubarMenu>
-            </Menubar>
-            <div>
-            <ResizablePanelGroup direction="horizontal" className='h-full'>
-                <ResizablePanel>
-                    <Table>
-                        <TableCaption>A list of your recent invoices.</TableCaption>
-                        <TableHeader>
-                            <TableRow>
-                            <TableHead className="w-[100px]">Files</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            <TableRow>
-                            <TableCell className="font-medium">Filename</TableCell>
-                            </TableRow>
-                            <TableRow>
-                            <TableCell className="font-medium">Filename</TableCell>
-                            </TableRow>
-                            <TableRow>
-                            <TableCell className="font-medium">Filename</TableCell>
-                            </TableRow>
-                            <TableRow>
-                            <TableCell className="font-medium">Filename</TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </ResizablePanel>
-                <ResizableHandle/>
-                <ResizablePanel>
-                    <Editor/>
-                </ResizablePanel>
-            </ResizablePanelGroup>
+        <div className="min-h-screen">
+            <div className="fixed top-0 left-0 right-0 bottom-0">
+                <Menubar>
+                <MenubarMenu>
+                    <MenubarTrigger>File</MenubarTrigger>
+                    <MenubarContent>
+                    <MenubarItem>
+                        New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                    </MenubarItem>
+                    <MenubarItem>New Window</MenubarItem>
+                    <MenubarSeparator />
+                    <MenubarItem>Share</MenubarItem>
+                    <MenubarSeparator />
+                    <MenubarItem>Print</MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>Edit</MenubarTrigger>
+                    <MenubarContent>
+                    <MenubarItem>
+                        New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                    </MenubarItem>
+                    <MenubarItem>New Window</MenubarItem>
+                    <MenubarSeparator />
+                    <MenubarItem>Share</MenubarItem>
+                    <MenubarSeparator />
+                    <MenubarItem>Print</MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>View</MenubarTrigger>
+                    <MenubarContent>
+                    <MenubarItem>
+                        New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                    </MenubarItem>
+                    <MenubarItem>New Window</MenubarItem>
+                    <MenubarSeparator />
+                    <MenubarItem>Share</MenubarItem>
+                    <MenubarSeparator />
+                    <MenubarItem>Print</MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+                </Menubar>
+                <div>
+                <ResizablePanelGroup direction="horizontal" className='h-full'>
+                    <ResizablePanel>
+                        <Table>
+                            <TableCaption>A list of your recent invoices.</TableCaption>
+                            <TableHeader>
+                                <TableRow>
+                                <TableHead className="w-[100px]">Files</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                <TableRow>
+                                <TableCell className="font-medium">Filename</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="font-medium">Filename</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="font-medium">Filename</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="font-medium">Filename</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="font-medium">Filename</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </ResizablePanel>
+                    <div className="relative">
+                        <div className="absolute inset-y-0 right-0 z-10">
+                            <ResizableHandle
+                            className="h-full"
+                            data-panel-group-direction="horizontal"
+                            />
+                        </div>
+                    </div>
+                    <ResizablePanel>
+                        <Editor/>
+                    </ResizablePanel>
+                </ResizablePanelGroup>
+                </div>
             </div>
-        </main>
+        </div>
     );
 };
 
